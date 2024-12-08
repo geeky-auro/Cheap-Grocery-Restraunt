20f2e632647520a42487e7a951d8902e89a915dc
geeky-auro <aurosaswat.apk@gmail.com>
2024-12-09 02:11:06 +0530

    End of Life-Cycle and Component Based Learning

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,27 @@
-const About=()=>{
-    return (<>
-        <h1>About</h1>
-        <h2>This is some Cheap Grocery</h2>
-    </>)
-};
+import { Component } from "react";
+import User from "./User";
+import UserClassComponent from "./UserClassComponent";
 
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor is Called");
+  }
+
+  componentDidMount() {
+    console.log("Parent ComponentDidMount is Called");
+  }
+
+  render() {
+    console.log("Parent Render is Called");
+    return (
+      <>
+        <h1>About</h1>
+        <User />
+        <UserClassComponent />
+      </>
+    );
+  }
+}
 
 export default About;
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h3>Name : Auro</h3>
+      <h3>Email : auro@example.com</h3>
+      <h3>Phone : 1234567890</h3>
+      <h3>Address : 123 Main St, City, State, Zip</h3>
+    </div>
+  );
+};
+
+export default User;
