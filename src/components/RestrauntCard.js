79ddb6c99115708057b1d53c0aa25e32ee2cd42b
geeky-auro <aurosaswat.apk@gmail.com>
2024-12-13 01:07:25 +0530

    Introduction to Tailwind CSS - On hover feature

--- src/components/RestrauntCard.js
+++ src/components/RestrauntCard.js
@@ -1,7 +1,7 @@
 import { styleCard } from "./Style";
 const RestrauntCard = ({ resData }) => {
   return (
-    <div className="m-4 p-4 w-[200px] rounded-lg" style={styleCard}>
+    <div className="m-4 p-4 w-[200px] rounded-lg bg-gray-100 hover:bg-gray-200">
       <img className="rounded-lg" src={resData.thumbnail} />
       <h3 className="font-bold py-4 text-lg">{resData.title}</h3>
       <h4>Description : {resData.description}</h4>
